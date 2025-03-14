a5a70ea1525b8699d84927c15fa870bc96ba4b2a
samuelhsieh0829 <66865408+samuelhsieh0829@users.noreply.github.com>
2025-03-14 17:36:38 +0800

    update

---
+++ app2.py
@@ -0,0 +1,36 @@
+import mod
+
+def main():
+    obj = mod.moduleA(6) # 正常值
+    obj1 = mod.moduleA(-100) # 負數
+    obj2 = mod.moduleA() # 預設值
+    obj3 = mod.moduleA(0) # 0
+    obj4 = mod.moduleA(0.5) # 浮點數
+    obj5 = mod.moduleA("WTF") # 字串
+    for i in range(2):
+        obj.methodA()
+        obj1.methodA()
+        obj2.methodA()
+        obj3.methodA()
+        obj4.methodA()
+        obj5.methodA()
+    obj.methodB()
+    obj1.methodB()
+    obj2.methodB()
+    obj3.methodB()
+    obj4.methodB()
+    obj5.methodB()
+    obj.methodC(3) # 正常值
+    obj1.methodC(-69) # 負數
+    obj2.methodC() # 無參數
+    obj3.methodC(0) # 0
+    obj4.methodC(0.5) # 浮點數
+    obj5.methodC("WTF") # 字串
+
+if __name__ == "__main__":
+    try:
+        main()
+        print("Done")
+    except Exception as e:
+        print(e)
+        print("Failed")
--- mod.py
+++ mod.py
@@ -1,10 +1,19 @@
 class moduleA:
     def __init__(self, arg:int=5):
+        if self.check_arg(arg) == False:
+            print("Invalid argument")
+            return
         self.arg = arg
         self.count = 0
         print("Module initualized")
 
     def methodA(self):
+        if self.check_arg(self.arg) == False:
+            print("Invalid argument")
+            return
+        if self.check_arg(self.count) == False:
+            print("Invalid count")
+            return
         for i in range(self.arg):
             print("Hello", i)
             self.count += 1
@@ -13,5 +22,15 @@ class moduleA:
         print("Count:", self.count)
 
     def methodC(self, arg:int):
+        if self.check_arg(arg) == False:
+            print("Invalid argument")
+            return
         for i in range(arg):
             self.methodB()
+
+    def check_arg(self, arg):
+        if isinstance(arg, int) == False:
+            return False
+        if arg <= 0:
+            return False
+        return True
