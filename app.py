6acd8f14855949610ac4f1c273656eea0c200419
samuelhsieh0829 <66865408+samuelhsieh0829@users.noreply.github.com>
2025-04-05 23:07:00 +0800

    update

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
-import mod
+from mod import moduleA
+import logging
 
-def main():
-    obj1 = mod.moduleA(2)
-    obj2 = mod.moduleA()
-    for i in range(2):
+logging.basicConfig(level=logging.INFO)
+
+def main(A, B, C):
+    obj1 = moduleA(A)
+    obj2 = moduleA()
+    for i in range(B):
         obj1.methodA()
         obj2.methodA()
     obj1.methodB()
     obj2.methodB()
-    obj1.methodC(3)
+    obj1.methodC(C)
     obj2.methodC()
 
 if __name__ == "__main__":
-    main()
+    score = 0
+
+    tests = [(2, 2, 3),
+             (1, 1, 1),
+             (1, 0, 1)]
+    
+    for test in tests:
+        try:
+            main(*test)
+            score += 1
+            logging.info(f"\x1b[32m{test} pass\x1b[0m")
+        except:
+            logging.exception(f"\x1b[31m{test} failed:\x1b[0m")
+        finally:
+            print("----------------------")
+
+    logging.info(f"\x1b[35mTest completed, score:\x1b[33m{score}\x1b[0m")
